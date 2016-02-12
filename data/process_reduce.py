c19d7bc3acd1dbea21292805fd6b79932ba7c614
Jaden Geller <jaden.geller@gmail.com>
2016-02-11 23:24:38 -0800

    Optimization: stop once the rank of the top 30 nodes stabilizes

--- data/process_reduce.py
+++ data/process_reduce.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
 
 import sys
+import numpy
+import itertools
 
 from collections import namedtuple
 
 Line = namedtuple('Line', 'node_num iter_num rank pr prev_pr connected_nodes')
 NUM_ITERS = 50
+NUM_STABLE = 30
+
+def top_k_indices_and_rest(ranks, k):
+    partitioned_indices = numpy.argpartition(ranks, -k)
+    unordered_top_k_indices = partitioned_indices[-k:]
+    unordered_top_k_ranks = ranks[unordered_top_k_indices]
+    ascending_top_k_indices = unordered_top_k_indices[numpy.argsort(unordered_top_k_ranks)]
+    return (ascending_top_k_indices, partitioned_indices[:-k])
 
 def parse_line(line):
     l = line.strip().split("\t")
@@ -21,6 +31,8 @@ def parse_line(line):
     prev_pr = float(value[3])
     connected_nodes = map(int, value[4:]) if len(value) > 3 else []
     return Line(node_num, iter_num, rank, pr, prev_pr, connected_nodes)
+
+
 
 def stringify_Line(l):
     output = "NodeId:" + str(l.node_num) + "\t"
@@ -48,14 +60,28 @@ for line in sys.stdin:
     l = parse_line(line)
     final_output.append(l)
 
-avg_diff = sum([abs(line.pr - line.prev_pr) for line in final_output]) / len(final_output)
+all_page_ranks = numpy.fromiter(itertools.imap(lambda l: l.pr, final_output), numpy.float, count=len(final_output))
+(top_indices, rest_indices) = top_k_indices_and_rest(all_page_ranks, NUM_STABLE)
 
-if avg_diff < 0.00001 or final_output[0].iter_num == NUM_ITERS:
-    # sort in descending order by pagerank
-    final_output = sorted(final_output, key=lambda line: line.pr, reverse=True)
+# Update all the ranks
+changed = False
+rank = NUM_STABLE - 1
+for i in top_indices:
+    if final_output[i].rank != rank:
+        final_output[i] = final_output[i]._replace(rank=rank)
+        changed = True
+    rank -= 1
 
-    for l in final_output[:20]:
+if not changed or final_output[0].iter_num == NUM_ITERS:
+    for i in reversed(top_indices):
+        l = final_output[i]
         print "FinalRank:" + str(l.pr) + "\t" + str(l.node_num)
+
 else:
+    # Clear ranks that were not updated
+    for i in rest_indices:
+        final_output[i] = final_output[i]._replace(rank=-1)
+
     for l in final_output:
         sys.stdout.write(stringify_Line(l))
+
