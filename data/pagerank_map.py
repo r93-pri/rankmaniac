d1056e10b5d8ebbb71f4c45dc6f1b9afb85a8d38
r93-pri <r93.priyanka@gmail.com>
2016-11-10 12:00:02 -0600

    Update pagerank_map.py

--- data/pagerank_map.py
+++ data/pagerank_map.py
@@ -4,28 +4,25 @@ import sys
 
 from collections import namedtuple
 
-Line = namedtuple('Line', 'node_num iter_num rank pr prev_pr connected_nodes')
+Line = namedtuple('node_num', 'connected_nodes')
 
 def parse_line(line):
     l = line.strip().split("\t")
     assert len(l) == 2
     key = l[0]
     value = l[1]
-    assert key.startswith("NodeId:")
-    node_num = int(key[7:])
-    if not value.startswith("i"):  #First iteration!
-        value = "i0,r-1," + value   # On the first iteration we add an iteration marker and an initial rank
     value = value.split(",")
-    iter_num = int(value[0][1:])
-    rank = int(value[1][1:])
-    pr = float(value[2])
-    prev_pr = float(value[3])
-    connected_nodes = map(int, value[4:]) if len(value) > 3 else []
-    return Line(node_num, iter_num, rank, pr, prev_pr, connected_nodes)
+    pr=0.001
+    #iter_num = int(value[0][1:])
+    #rank = int(value[1][1:])
+    #pr = float(value[2])
+    #prev_pr = float(value[3])
+    #connected_nodes = map(int, value[4:]) if len(value) > 3 else []
+    return Line(node_num, connected_nodes, pr)
 
 def stringify_Line(l):
     output = "NodeId:" + str(l.node_num) + "\t"
-    output += "i" + str(l.iter_num) + ",r" + str(l.rank) + "," + str(l.pr) + "," + str(l.prev_pr)
+    output += "i:" + str(l.iter_num) + ",pr:" + str(l.pr) + "," + str(l.prev_pr)
     if len(l.connected_nodes) > 0:
         output += "," + (','.join(map(str, l.connected_nodes)))
     output += "\n"
@@ -45,6 +42,6 @@ def map_pr(l):
 lines = []
 for line in sys.stdin:
     l = parse_line(line)
-    l = l._replace(iter_num=(l.iter_num + 1))  # We increment the number of iterations each time
+    #l = l._replace(iter_num=(l.iter_num + 1))  # We increment the number of iterations each time
     map_pr(l) # add node's pagerank / degree to each page it links to
     sys.stdout.write(stringify_Line(l))
